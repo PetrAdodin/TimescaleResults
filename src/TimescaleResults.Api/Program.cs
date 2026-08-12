c0d96d2a7ba0fdbe1bd9c029218247728f5138c5
PetrAdodin <petradodin@gmail.com>
2026-08-12 12:23:23 +0700

    feat: add Swagger UI

--- src/TimescaleResults.Api/Program.cs
+++ src/TimescaleResults.Api/Program.cs
@@ -35,6 +35,11 @@ app.UseExceptionHandler();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "TimescaleResults API v1");
+    });
 }
 
 app.UseHttpsRedirection();
